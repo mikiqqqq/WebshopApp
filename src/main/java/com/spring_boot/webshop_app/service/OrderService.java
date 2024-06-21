7ac020c111b730ffa9f98d2d214752c85569aa59
Filip Miloš <136697753+filipMiloshh@users.noreply.github.com>
2024-06-21 19:15:38 +0200

    updates

--- src/main/java/com/spring_boot/webshop_app/service/OrderService.java
+++ src/main/java/com/spring_boot/webshop_app/service/OrderService.java
@@ -1,12 +1,14 @@
 package com.spring_boot.webshop_app.service;
 
-import com.spring_boot.webshop_app.enumeration.OrderStatus;
 import com.spring_boot.webshop_app.model.Order;
+
+import java.util.List;
 
 public interface OrderService {
     Order fetchOrderById (Integer id);
     Integer save (Order order);
-    Order fetchOrderByOrderStatus  ();
+    List<Order> fetchActiveOrders ();
+    List<Order> fetchCompletedOrders ();
     void update (Integer id, Order updatedOrder);
     void delete (Integer id);
 }
